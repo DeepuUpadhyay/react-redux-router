8f6e53c36875ad62d612bd14c7c139638d6f1dc4
Shivam Upadhyay <shivam4468@gmail.com>
2020-02-21 22:01:21 +0530

    ALL POC at once place
    All Poc

---
+++ 03/demos/before/src/App.js
@@ -0,0 +1,5 @@
+var HelloWord = React.createClass({
+ render: function() {
+  return <h1>HelloWord</h1>;
+ }
+});
---
+++ 03/demos/before/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+const index = () => {
+ return <p>hi</p>;
+};
+render(<index />, document.getElementById("app"));
